cb4af2c6a45ee555559dee7c42b6dfff542eca7a
Arpan Chhetri <Arpan.Chhetri@kiwibank.co.nz>
2024-09-23 15:39:51 +1200

    convert line ending

--- scripts/required-packages.sh
+++ scripts/required-packages.sh
@@ -7,8 +7,4 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utils.sh
 distro=$(whichdistro)
 if [[ $distro == "redhat" ]]; then
 	checkinstall findutils
-elif
-	echo "os not compatible"; then
-	exit 1
-	return
 fi
