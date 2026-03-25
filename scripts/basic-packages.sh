ffe62a098684ae10c83ead52ec640b02abb93ed1
Arpan Chhetri <arpan.chhetri@kiwibank.co.nz>
2026-03-25 17:02:06 +1300

    feat: promote helix to core and install during bootstrap
    - Move helix config from config/optional/helix to config/core/helix
    - Update full and hypr-minimal manifests to link config/core/helix
    - Add helix package to bootstrap installation list across distro branches
    - Keep linking behavior consistent for new machine bootstrap

--- scripts/basic-packages.sh
+++ scripts/basic-packages.sh
@@ -6,11 +6,11 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utils.sh
 
 distro=$(whichdistro)
 if [[ $distro == "redhat" ]]; then
-	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip sqlite sqlite-devel gettext procps jq
+	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip sqlite sqlite-devel gettext procps jq helix
 elif [[ $distro == "debian" ]]; then
-	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip sqlite3 gettext procps jq
+	checkinstall zsh git tmux bc curl wget gawk python3-pip unzip sqlite3 gettext procps jq helix
 else
-	checkinstall zsh git tmux bc curl wget xsel gawk python-pip unzip sqlite gettext procps jq
+	checkinstall zsh git tmux bc curl wget xsel gawk python-pip unzip sqlite gettext procps jq helix
 fi
 
 if [[ $distro == "redhat" ]]; then
